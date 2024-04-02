8af36d45a06c9ed67872f98ed8039dc54f18caf3
dafnygc <129951774+dafnygc@users.noreply.github.com>
2024-04-02 10:49:51 -0300

    Update senha.py

--- senha.py
+++ senha.py
@@ -1,3 +1,4 @@
+               SENHA 
 senha = "1234"
 
 qsenha = input("Digite sua senha: ")
@@ -6,4 +7,18 @@ if (senha == qsenha):
     print("acesso permitido")
 else:
     print("acesso negado")
-     
+
+
+            MACA
+maca = int()
+qmaca = int(input("Digite a quantia e maaas que deseja comprar"))
+
+if qmaca < 12:
+    maca = 0.30
+ else:
+     maca = 0.25
+     print("o valor total é",qmaca * maca)
+
+
+            
+            
