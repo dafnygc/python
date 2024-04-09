dd428b326f8fe64e68de57277b98c598026e41fd
dafnygc <129951774+dafnygc@users.noreply.github.com>
2024-04-09 12:21:37 -0300

    Update senha.py

--- senha.py
+++ senha.py
@@ -19,6 +19,18 @@ if qmaca < 12:
      maca = 0.25
      print("o valor total é",qmaca * maca)
 
+n1 = int (input ("escolha o primeiro numero"))
+n2 = int (input ("escolha o segundo numero"))
+n3 = int (input ("escolha o terceiro numero"))
+
+if n1 < n2 and n1 < n2 
+  p1 = n1
+  if n2< n1 and n2 < n3 
+  p1 = n2
+  else:
+    p1 = n3
+
+
 
             
             
